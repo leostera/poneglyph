4714fd4dba009f89c58e3b6c7e5b6e2998605bb3
Leandro Ostera <leandro@abstractmachines.dev>
2026-03-13 16:31:29 +0100

    feat(datafox): support conjunctive query evaluation

--- crates/datafox/src/evaluator.rs
+++ crates/datafox/src/evaluator.rs
@@ -10,12 +10,19 @@ impl Evaluator {
         universe: &'a Universe<S>,
         atom: &'a Atom,
     ) -> Result<SubstitutionStream<'a>> {
-        let pattern = atom_to_pattern(atom);
-        let atom = atom.clone();
+        Self::query_atom(universe, atom.clone(), Substitution::new())
+    }
+
+    fn query_atom<'a, S: Storage>(
+        universe: &'a Universe<S>,
+        atom: Atom,
+        seed: Substitution,
+    ) -> Result<SubstitutionStream<'a>> {
+        let pattern = atom_to_pattern(&seed.apply_to_atom(&atom));
         let tuples = universe.get_facts_matching(&atom.predicate, pattern)?;
 
         Ok(Box::new(tuples.filter_map(move |tuple| match tuple {
-            Ok(tuple) => match Unifier::match_atom(&Substitution::new(), &atom, &tuple) {
+            Ok(tuple) => match Unifier::match_atom(&seed, &atom, &tuple) {
                 Ok(Some(substitution)) => Some(Ok(substitution)),
                 Ok(None) => None,
                 Err(error) => Some(Err(error)),
@@ -30,21 +37,37 @@ impl Evaluator {
     ) -> Result<SubstitutionStream<'a>> {
         match query {
             Query::Single(atom) => Self::query(universe, atom),
-            Query::Multi(clauses) => {
-                if clauses
-                    .iter()
-                    .any(|clause| matches!(clause, Clause::Negated(_)))
-                {
-                    return Err(Error::UnsupportedNegation);
-                }
-                if clauses
-                    .iter()
-                    .any(|clause| matches!(clause, Clause::Builtin { .. }))
-                {
-                    return Err(Error::UnsupportedBuiltin);
-                }
-                Err(Error::UnsupportedMultiQuery)
+            Query::Multi(clauses) => Self::evaluate_clauses(universe, Substitution::new(), clauses),
+        }
+    }
+
+    fn evaluate_clauses<'a, S: Storage>(
+        universe: &'a Universe<S>,
+        seed: Substitution,
+        clauses: &'a [Clause],
+    ) -> Result<SubstitutionStream<'a>> {
+        let Some((first, rest)) = clauses.split_first() else {
+            return Ok(Box::new(std::iter::once(Ok(seed))));
+        };
+
+        match first {
+            Clause::Atom(atom) => {
+                let stream = Self::query_atom(universe, atom.clone(), seed)?;
+
+                Ok(Box::new(stream.flat_map(move |result| match result {
+                    Ok(substitution) => {
+                        match Self::evaluate_clauses(universe, substitution, rest) {
+                            Ok(stream) => stream,
+                            Err(error) => {
+                                Box::new(std::iter::once(Err(error))) as SubstitutionStream<'a>
+                            }
+                        }
+                    }
+                    Err(error) => Box::new(std::iter::once(Err(error))) as SubstitutionStream<'a>,
+                })))
             }
+            Clause::Negated(_) => Err(Error::UnsupportedNegation),
+            Clause::Builtin { .. } => Err(Error::UnsupportedBuiltin),
         }
     }
 }
@@ -103,8 +126,63 @@ mod tests {
     }
 
     #[test]
-    fn evaluator_rejects_multi_goal_queries_for_now() -> Result<()> {
-        let universe = Universe::new(InMemoryStorage::new());
+    fn evaluator_streams_multi_goal_conjunctive_matches() -> Result<()> {
+        let universe = Universe::new(InMemoryStorage::from_facts([
+            (
+                "spotify:byArtist".to_string(),
+                vec![
+                    vec![
+                        Value::from("spotify:album:2112"),
+                        Value::from("spotify:artist:rush"),
+                    ],
+                    vec![
+                        Value::from("spotify:album:fragile"),
+                        Value::from("spotify:artist:yes"),
+                    ],
+                ],
+            ),
+            (
+                "spotify:displayName".to_string(),
+                vec![
+                    vec![Value::from("spotify:artist:rush"), Value::from("Rush")],
+                    vec![Value::from("spotify:artist:yes"), Value::from("Yes")],
+                ],
+            ),
+        ]));
+        let query = Query::multi(vec![
+            Clause::atom(crate::atom!(
+                "spotify:byArtist",
+                vec![crate::var!("Album"), crate::var!("Artist")]
+            )),
+            Clause::atom(crate::atom!(
+                "spotify:displayName",
+                vec![crate::var!("Artist"), crate::lit!(Value::from("Rush"))]
+            )),
+        ])?;
+
+        let results = Evaluator::evaluate(&universe, &query)?.collect::<crate::Result<Vec<_>>>()?;
+
+        assert_eq!(results.len(), 1);
+        assert_eq!(
+            results[0].lookup("Album"),
+            Some(&Value::from("spotify:album:2112"))
+        );
+        assert_eq!(
+            results[0].lookup("Artist"),
+            Some(&Value::from("spotify:artist:rush"))
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn evaluator_streams_empty_results_for_unsatisfied_multi_goal_queries() -> Result<()> {
+        let universe = Universe::new(InMemoryStorage::from_facts([(
+            "edge".to_string(),
+            vec![
+                vec![Value::integer(1), Value::integer(2)],
+                vec![Value::integer(2), Value::integer(3)],
+            ],
+        )]));
         let query = Query::multi(vec![
             Clause::atom(crate::atom!(
                 "edge",
@@ -112,16 +190,13 @@ mod tests {
             )),
             Clause::atom(crate::atom!(
                 "edge",
-                vec![crate::var!("Y"), crate::var!("Z")]
+                vec![crate::var!("Y"), crate::lit!(Value::integer(99))]
             )),
         ])?;
 
-        let error = match Evaluator::evaluate(&universe, &query) {
-            Ok(_) => panic!("expected unsupported multi query"),
-            Err(error) => error,
-        };
+        let results = Evaluator::evaluate(&universe, &query)?.collect::<crate::Result<Vec<_>>>()?;
 
-        assert_eq!(error, crate::Error::UnsupportedMultiQuery);
+        assert!(results.is_empty());
         Ok(())
     }
 
